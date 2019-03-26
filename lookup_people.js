606bc7afbac449068e0f641651d357bbf5fb64df
boksul <boksul@hotmail.com>
2019-03-26 17:56:46 +0000

    first commit

---
+++ lookup_people.js
@@ -0,0 +1,40 @@
+const pg = require('pg');
+const settings = require('./settings'); // settings.json
+
+const client = new pg.Client({
+    user     : settings.user,
+    password : settings.password,
+    database : settings.database,
+    host     : settings.hostname,
+    port     : settings.port,
+    ssl      : settings.ssl
+  });
+
+
+function search() {
+    const input = process.argv[2];
+    const query = `SELECT * FROM famous_people`;
+    let found = 0;
+    let result = [];
+    client.query(query, (err, res) => {
+        if (err) throw err;
+        for (let person of res.rows) {
+            if (person.first_name === input || person.last_name === input) {
+                found++;
+                result.push(person);
+            }
+        }
+        console.log('Searching...');
+        console.log(`Found ${result.length} person(s) by the name ${input}:`);
+        result.forEach((a, b) => {
+            console.log(`-${b + 1}: ${a.first_name} ${a.last_name} ${a.birthdate}`)
+        });
+    client.end();
+    })
+}
+client.connect((err) => {
+    if (err) {
+        return console.error("Connection Error", err);
+    }
+    search();
+});
--- test_script.js
+++ test_script.js
@@ -1,20 +1,20 @@
 const pg = require("pg");
 const settings = require("./settings"); // settings.json
 
-const client = new pg.Client({
-  user     : settings.user,
-  password : settings.password,
-  database : settings.database,
-  host     : settings.hostname,
-  port     : settings.port,
-  ssl      : settings.ssl
+const client = new pg.Client({  //creates reference to database
+    user:     settings.user,
+    password: settings.password,
+    database: settings.database,
+    host:     settings.hostname,
+    port:     settings.port,
+    ssl:      settings.ssl
 });
 
-client.connect((err) => {
+client.connect((err) => { //connect to database, pass in callback in case it doesn't connect
   if (err) {
     return console.error("Connection Error", err);
   }
-  client.query("SELECT $1::int AS number", ["1"], (err, result) => {
+  client.query("SELECT $1::int AS number", ["1"], (err, result) => { // if it works, pass in a query (if you have parameters, pass them in with placeholders, like follows, and pass in as array)
     if (err) {
       return console.error("error running query", err);
     }
